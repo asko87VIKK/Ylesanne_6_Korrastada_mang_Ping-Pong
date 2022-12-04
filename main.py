65d20107c8c57e9fb0d2463718d57fd32b495646
Asko Aru <asko.aru@vikk.ee>
2022-12-04 16:03:02 +0200

    muudatused

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Asko Aru
 
 # Pildid peavad asuma kaustas: pildi_failid
+# Tausta muusika peab asuma kaustas: heli
 
 import pygame  # impordime pygame mooduli
 import random  # impordime random mooduli
